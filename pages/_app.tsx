83cc1cde499a9725676d8fc10ed5a70fae462c3c
Rafael <rafaeloliveiracds@gmail.com>
2023-02-28 03:46:53 -0300

    [ configs ] styled components

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+  body {
+    margin: 1rem;
+    background-color: #101010 ;
+    color: #eee
+  }
+`;
+
+// interface ThemeInterface {
+//   colors: {
+//     primary: string;
+//   };
+// }
+
+// const theme: ThemeInterface = {
+//   colors: {
+//     primary: "#0070f3",
+//   },
+// };
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
