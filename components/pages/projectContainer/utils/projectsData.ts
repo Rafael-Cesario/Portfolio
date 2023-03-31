590c1d55133feffe271d7bf8b5c067afb917b3fb
Rafael <rafaeloliveiracds@gmail.com>
2023-03-31 04:12:00 -0300

    Projects // new project

--- components/pages/projectContainer/utils/projectsData.ts
+++ components/pages/projectContainer/utils/projectsData.ts
@@ -1,57 +1,65 @@
 type project = {
-  name: string;
-  txt: string;
-  stack: string;
-  scope: string;
-  github: string;
+	name: string;
+	txt: string;
+	stack: string;
+	scope: string;
+	github: string;
 };
 
 export const projectsData: project[] = [
-  {
-    name: "WordList",
-    txt: "WordList é um site feito para te ajudar a aprender um idioma, mais precisamente para te ajudar a memorizar algumas palavras.",
-    stack: "nextjs, styled-components, graphql, mongodb, vitest, typescript",
-    scope: "FullStack",
-    github: "http://github.com/rafael-cesario/wordlist",
-  },
+	{
+		name: 'To Do List FullStack',
+		txt: 'Create lists, todos and tags to keep yourself organized while working or studying something.',
+		stack: 'React, react-router, typescript, graphql, apollo, vite, vitest, jwt, mongodb, styled-components',
+		scope: 'FullStack',
+		github: 'https://github.com/Rafael-Cesario/To-Do-List-Full-Stack',
+	},
 
-  {
-    name: "Algorithms",
-    txt: "Uma coleção de algoritmos.",
-    stack: "Javascript",
-    scope: "",
-    github: "https://github.com/Rafael-Cesario/Algorithms",
-  },
+	{
+		name: 'WordList',
+		txt: 'WordList é um site feito para te ajudar a aprender um idioma, mais precisamente para te ajudar a memorizar algumas palavras.',
+		stack: 'nextjs, styled-components, graphql, mongodb, vitest, typescript',
+		scope: 'FullStack',
+		github: 'http://github.com/rafael-cesario/wordlist',
+	},
 
-  {
-    name: "Portfólio",
-    txt: "Meu site pessoal para salvar meus projetos.",
-    stack: "nextjs, styled-components, vitest, typescript",
-    scope: "Frontend",
-    github: "http://github.com/rafael-cesario/portfolio",
-  },
+	{
+		name: 'Algorithms',
+		txt: 'Uma coleção de algoritmos.',
+		stack: 'Javascript',
+		scope: '',
+		github: 'https://github.com/Rafael-Cesario/Algorithms',
+	},
 
-  {
-    name: "Login Page",
-    txt: "Uma pagina para fazer login ou criar uma conta.",
-    stack: "nextjs, graphql, tailwindcss, typescript, mongodb, JWT",
-    scope: "FullStack",
-    github: "https://github.com/Rafael-Cesario/Projects/tree/main/08-login",
-  },
+	{
+		name: 'Portfólio',
+		txt: 'Meu site pessoal para salvar meus projetos.',
+		stack: 'nextjs, styled-components, vitest, typescript',
+		scope: 'Frontend',
+		github: 'http://github.com/rafael-cesario/portfolio',
+	},
 
-  {
-    name: "User Crud - Graphql",
-    txt: "Um simples crud de usuario utilizando graphql.",
-    stack: "graphql, mongodb, typescript, vitest",
-    scope: "backend",
-    github: "https://github.com/Rafael-Cesario/Projects/tree/main/05-user-crud-graphql",
-  },
+	{
+		name: 'Login Page',
+		txt: 'Uma pagina para fazer login ou criar uma conta.',
+		stack: 'nextjs, graphql, tailwindcss, typescript, mongodb, JWT',
+		scope: 'FullStack',
+		github: 'https://github.com/Rafael-Cesario/Projects/tree/main/08-login',
+	},
 
-  {
-    name: "Projects",
-    txt: "Alguns pequenos projetos focados em algo especifico.",
-    stack: "",
-    scope: "FullStack",
-    github: "https://github.com/Rafael-Cesario/Projects",
-  },
+	{
+		name: 'User Crud - Graphql',
+		txt: 'Um simples crud de usuario utilizando graphql.',
+		stack: 'graphql, mongodb, typescript, vitest',
+		scope: 'backend',
+		github: 'https://github.com/Rafael-Cesario/Projects/tree/main/05-user-crud-graphql',
+	},
+
+	{
+		name: 'Projects',
+		txt: 'Alguns pequenos projetos focados em algo especifico.',
+		stack: '',
+		scope: 'FullStack',
+		github: 'https://github.com/Rafael-Cesario/Projects',
+	},
 ];
