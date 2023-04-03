d80104e6b84b9ebd7dce3fbf3d75b09a84e8ecf2
Rafael <rafaeloliveiracds@gmail.com>
2023-04-03 14:09:34 -0300

    Feat // Projects page description

--- components/pages/projectContainer/styles/styledTitle.tsx
+++ components/pages/projectContainer/styles/styledTitle.tsx
@@ -1,39 +1,43 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledTitle = styled.div`
-  margin: 5rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	margin: 10rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	max-width: 60rem;
 
-  .title {
-    margin: 1rem;
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: #ddd;
-    position: relative;
+	.title {
+    margin: 0.5rem;
+		font-size: 1.5rem;
+		font-weight: bold;
+		color: #ddd;
+		position: relative;
 
-    :after {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      background-color: #101010;
-      width: 5rem;
-      height: 0.5rem;
-      padding: 1rem;
-      animation: typing 1s both;
-    }
+		:after {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0;
+			background-color: #101010;
+			width: 5rem;
+			height: 0.5rem;
+			padding: 1rem;
+			animation: typing 1s both;
+		}
 
-    @keyframes typing {
-      to {
-        transform: translate(10rem, 0);
-        display: none;
-      }
-    }
-  }
+		@keyframes typing {
+			to {
+				transform: translate(10rem, 0);
+				display: none;
+			}
+		}
+	}
 
-  .para {
-    color: #aaa;
-  }
+	p {
+		color: #aaa;
+		line-height: 1.5rem;
+		text-align: justify;
+		margin: 0.5rem 0;
+	}
 `;
--- components/pages/projectContainer/title.tsx
+++ components/pages/projectContainer/title.tsx
@@ -1,9 +1,18 @@
-import { StyledTitle } from "./styles/styledTitle";
+import { StyledTitle } from './styles/styledTitle';
 
 export const Title = () => {
-  return (
-    <StyledTitle>
-      <h1 className="title">Projetos</h1>
-    </StyledTitle>
-  );
+	return (
+		<StyledTitle>
+			<h1 className="title">Projetos</h1>
+			<p>
+				Criar um projeto, continuar usando ele no dia a dia e anotar detalhes, design ou erros, tem sido meu melhor meio de melhorar como
+				desenvolvedor.
+			</p>
+
+			<p>
+				Alguns projetos são feitos enquanto estou estudando, eles são como anotações de estudo, outros projetos são para facilitar os meus dias, mesmo
+				que outro site parecido já exista, não é o mesmo que ter o seu próprio, sem contar o quanto eu aprendo criando eles.
+			</p>
+		</StyledTitle>
+	);
 };
