9b041287f594d3826a878c69b4533a02eb0a6734
Rafael <rafaeloliveiracds@gmail.com>
2023-05-15 10:50:06 -0300

    New project gameshelf

--- components/pages/projectContainer/utils/projectsData.ts
+++ components/pages/projectContainer/utils/projectsData.ts
@@ -8,6 +8,14 @@ type project = {
 
 export const projectsData: project[] = [
 	{
+		name: 'GameShelf',
+		txt: 'Save your games, add tags, genre, rate, images, and have a organized place to see your games.',
+		stack: 'Nextjs, React, Styled-components, graphql, apollo-server, vitest, jwt, eslint, mongodb, typegoose, mongoose',
+		scope: 'FullStack',
+		github: 'https://github.com/Rafael-Cesario/GameShelf',
+	},
+
+	{
 		name: 'To Do List FullStack',
 		txt: 'Create lists, todos and tags to keep yourself organized while working or studying something.',
 		stack: 'React, react-router, typescript, graphql, apollo, vite, vitest, jwt, mongodb, styled-components',
@@ -18,7 +26,7 @@ export const projectsData: project[] = [
 	{
 		name: 'WordList',
 		txt: 'WordList é um site feito para te ajudar a aprender um idioma, mais precisamente para te ajudar a memorizar algumas palavras.',
-		stack: 'nextjs, styled-components, graphql, mongodb, vitest, typescript',
+		stack: 'Nextjs, styled-components, graphql, mongodb, vitest, typescript',
 		scope: 'FullStack',
 		github: 'http://github.com/rafael-cesario/wordlist',
 	},
@@ -49,7 +57,7 @@ export const projectsData: project[] = [
 
 	{
 		name: 'User Crud - Graphql',
-		txt: 'Um simples crud de usuario utilizando graphql.',
+		txt: 'Um simples crud de usuário utilizando graphql.',
 		stack: 'graphql, mongodb, typescript, vitest',
 		scope: 'backend',
 		github: 'https://github.com/Rafael-Cesario/Projects/tree/main/05-user-crud-graphql',
