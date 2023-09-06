1a360c3c49a3be4c85552b6206c80ecdb69e1b2f
Rafael <rafaeloliveiracds@gmail.com>
2023-09-06 19:29:23 -0300

    Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import type { Metadata } from "next";
+import { Roboto_Slab } from "next/font/google";
 
 export const metadata: Metadata = {
 	title: "Rafael",
 	description: "My personal portfolio",
 };
 
+const roboto_slab = Roboto_Slab({
+	display: "swap",
+	subsets: ["latin"],
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="pt-br">
+		<html lang="pt-br" className={roboto_slab.className}>
 			<body>{children}</body>
 		</html>
 	);
